0c80f8d55887df42d4624dbd52ede1fad62edc7e
AlfredGerke <alfred.gerke@software-aus-erwitte.de>
2018-05-30 22:24:45 +0200

    Neue Firebird Version
    ...

--- source/create_db.sql
+++ source/create_db.sql
@@ -16,7 +16,7 @@
 SHEll DEL C:\Users\Alfred\Sourcen\db\firebird\simplifyDDL\SIMPLIFYDDL.FDB;
 
 /* Entwicklungsdatenbank erstellen: Alias SIMPLIFYDDL anlegen! */
-CREATE DATABASE '127.0.0.1:SIMPLIFYDDL' USER 'SYSDBA' PASSWORD 'masterkey' PAGE_SIZE 4096 DEFAULT CHARACTER SET UTF8 COLLATION UTF8;
+CREATE DATABASE '127.0.0.1/32303:SIMPLIFYDDL' USER 'SYSDBA' PASSWORD 'masterkey' PAGE_SIZE 4096 DEFAULT CHARACTER SET UTF8 COLLATION UTF8;
 
 /*------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------*/  
--- source/make_sDDL.bat
+++ source/make_sDDL.bat
@@ -2,4 +2,4 @@ REM In make_sDDL.log wird jede verarbeitete Zeile aufgelistet
 REM Bei einem Fehler wird sofort die Verarbeitung unterbrochen
 REM Die letzte Zeile zeigt an, wo und welcher Fehler aufgetreten ist
 
-_make C:\Users\Alfred\Programme\Firebird_3_0\isql.exe make_sDDL.log *
+_make C:\Users\Alfred\Programme\Firebird\x32\v303\isql.exe make_sDDL.log *
