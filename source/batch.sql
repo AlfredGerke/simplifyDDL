4955be5973cf23a165c6742e425727ca86e3eec6
Alfred Gerke <alfred.gerke@gmail.com>
2020-03-16 13:43:16 +0100

    Umzug der lokalen Sourcen

--- source/batch.sql
+++ source/batch.sql
@@ -28,7 +28,7 @@ SET HEADING on;
 SET LIST on;
 
 /* Connect ---------------------------------------------------------------------------------------*/
-CONNECT '127.0.0.1:SIMPLIFYDDL' USER 'SYSDBA' PASSWORD 'masterkey';
+CONNECT '127.0.0.1/64302:SIMPLIFYDDL' USER 'SYSDBA' PASSWORD 'masterkey';
 
 /* Input -----------------------------------------------------------------------------------------*/
 input 'create_sequences.sql';
--- source/make_sDDL.bat
+++ source/make_sDDL.bat
@@ -2,4 +2,15 @@ REM In make_sDDL.log wird jede verarbeitete Zeile aufgelistet
 REM Bei einem Fehler wird sofort die Verarbeitung unterbrochen
 REM Die letzte Zeile zeigt an, wo und welcher Fehler aufgetreten ist
 
+if "%1"=="UES" goto UES
+
 _make C:\Users\Alfred\Programme\Firebird_3_0\isql.exe make_sDDL.log *
+
+goto ENDE
+
+:UES
+_make C:\Firebird\x64\v302\isql.exe make_sDDL.log
+
+pause
+
+:ENDE
